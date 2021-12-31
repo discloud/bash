fcd754bbba5729f26ab9554f3c13a27ae7ae479d
PR <pedroricardorn@gmail.com>
2021-12-30 21:55:41 -0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,9 +3,9 @@ if not "%1"=="am_admin" (powershell.exe start -verb runas '%0' am_admin & exit /
 
 if not exist "C:\Discloud\" mkdir C:\Discloud
 
-for /f "usebackq delims=" %%a in (`powershell.exe -Command "(new-object net.webclient).DownloadString('https://raw.githubusercontent.com/discloud/bash/main/system/windows/discloud.cmd')"`) do set variable=%%a
+SET "downloadurl=https://raw.githubusercontent.com/discloud/bash/main/system/windows/discloud.cmd"
 
-(echo %variable%)>C:\Discloud\discloud.bat
+powershell.exe -Command "Invoke-WebRequest -Uri %downloadurl% -OutFile C:\Discloud\discloud.bat"
 
 setx /M PATH "%PATH%;C:\Discloud"
 echo [32m Install finalizado, abra um novo terminal e digite discloud[0m
